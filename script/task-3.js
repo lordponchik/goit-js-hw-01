3b5b0c9574ac455b1a955c6ad497f89d0d20888d
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-10-30 13:22:53 +0100

    add task-3

---
+++ script/task-3.js
@@ -0,0 +1,16 @@
+console.log('');
+console.log('task-3');
+
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message = '';
+let password = prompt('Please enter your password');
+
+if (password === null) {
+  message = 'Отменено пользователем!';
+} else if (password === ADMIN_PASSWORD) {
+  message = 'Добро пожаловать!';
+} else {
+  message = 'Доступ запрещен, неверный пароль!';
+}
+
+alert(message);
